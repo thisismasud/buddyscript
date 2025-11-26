5d7dc08d756986a310a0bf33c521e3a53574b2c2
Masud Parvez <thisismasud1@gmail.com>
2025-11-26 19:51:02 +0600

    refactor

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -4,8 +4,8 @@ import checkLogin from "../middlewares/common/checkLogin";
 import {postImageUpload} from "../middlewares/posts/postImageUpload";
 const router = Router();
 
-router.post("/createpost", checkLogin, postImageUpload(), PostController.createPost);
-router.get("/get_posts", checkLogin, PostController.getPosts);
+router.post("/create_post", checkLogin, postImageUpload(), PostController.createPost);
+router.get("/", checkLogin, PostController.getPosts);
 
 
 export default router;
